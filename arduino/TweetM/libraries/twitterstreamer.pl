2889b9fab618588932a3d76f574ab69f3a7be6b5
Tod E. Kurt <todbot@users.noreply.github.com>
2010-06-11 00:52:35 +0000

    renamed WiFiBlinkM to TweetM

---
+++ arduino/TweetM/libraries/twitterstreamer.pl
@@ -0,0 +1,172 @@
+#!/usr/bin/env perl 
+
+#
+# twitterstreamer.pl -- 
+#
+#
+#
+#
+
+use strict;
+use warnings;
+
+use IO::Socket;
+use IO::Select;
+use MIME::Base64;
+#use Time::Hires qw(time);
+
+my $debug = 1;
+
+# change this to be your own Twitter login
+my $twuser = "blinkmlive";
+my $twpass = "redgrenblue";
+
+# only three are allowed
+my @keywords = 
+    (
+     "colbert,colbertreport,dailyshow,stephenathome", 
+     "grizzlies,grizzly,america",
+     "freedom,usa"
+    );
+
+# the file to write to
+#my $countsfile = "/home/83337/users/.home/todbot.com/colberttweets/index.php";
+my $countsfile = "index.php";
+
+my $keywords_counts = {};
+
+# how 
+my $duration = 5 * 59;  # 5 minutes, minus a little
+my $perduration = 10;
+my $selsecs = 1;
+
+
+
+my $twBaseUrl = "http://stream.twitter.com/1/statuses/filter.json";
+
+my $keywordstr = join( ',', grep { !"" } @keywords);
+$keywordstr =~ s/^,//; # sigh, remove leading comma, if any
+
+my $host = "stream.twitter.com";
+my $uri =  "/1/statuses/filter.json?track=$keywordstr";
+my $auth = encode_base64("$twuser:$twpass","");
+
+#
+sub debug($;$) {
+    my ($str,$lvl) = @_;
+    $lvl ||= 1;
+    print $str if($debug>=$lvl);
+}
+
+debug("uri:$uri\n");
+debug("auth: $auth\n");
+
+#
+sub updateFile() {
+
+    my $cstr = 
+	"<?php header('Content-type: text/plain'); ?>\n".
+        "// ".localtime(). "\n".
+        "// list of three different keywordsets tweet count\n".
+        "// each color count causes that many flashes.\n".
+        "// keywords: ";
+    foreach my $k (@keywords) {
+        $cstr .= $k .";  ";
+    }
+    $cstr .="\n";
+    $cstr .= "#";
+
+    # get counts
+    foreach my $k (@keywords) { 
+        my $v = $keywords_counts->{$k};
+        $cstr .= sprintf( "%02x", $v);
+        debug("$k : $v\n");
+    }
+
+    open( my $fh, '>', $countsfile) or die $!;  # FIXME
+    print $fh "$cstr\n";
+    close( $fh );
+}
+
+#
+sub getTweets() {
+    my $sock = IO::Socket::INET->new( Proto => 'tcp',
+                                      PeerAddr => $host,
+                                      PeerPort => 'http(80)',
+                                      timeout  => $perduration,
+        );
+    if( !$sock ) {
+        debug("couldn't connect");
+        return 0;
+    }
+    
+    print $sock "GET $uri HTTP/1.1\n";
+    print $sock "Host: $host\n";
+    print $sock "Authorization: Basic $auth\n";
+    print $sock "User-Agent: todbot.com twitterstreamer 0.5\n";
+    print $sock "Accept: */*\n";
+    print $sock "\n";
+    
+    my $starttime = time();
+    debug("getTweets: $starttime (". localtime() .")\n");
+    
+    my $sel = new IO::Select( $sock );
+    
+    my $maxt = $starttime + $perduration;
+    while( time() < $maxt ) {
+        if( $sel->can_read( $selsecs ) ) {
+            my $line = <$sock>;
+            debug("line: $line",2);
+            foreach my $k (@keywords) { 
+                foreach my $kk (split ',', $k) {  # in case of multi-word 
+                    $keywords_counts->{$k}++ if( $line =~ /$kk/i );
+                }
+            }
+        }
+    }
+    close($sock);
+    return 1;
+}
+
+my $startt = time();
+my $endt = $startt + $duration;
+while( time() < $endt ) {
+    foreach my $k (@keywords)  { 
+        $keywords_counts->{$k} = 0;  # reset
+    }
+    if( getTweets() ) {  # each of these takes $perduration seconds
+        updateFile();
+    }
+}
+
+
+
+#------------------------------------------------------------
+
+#while( my $line = <$sock> ) { 
+#    printf "len: %x\n", length($line);
+#    print $line;
+#    my $t = time();
+#    print "time: $t\n";
+#}
+
+
+
+#sub encode_base64 { 
+#    use integer;
+#    my $res = ""; 
+#    pos( $_[0] )= 0; 
+#    while ( $_[0] =~ /(.{1,45})/gs ) {
+#        $res .= substr( pack('u', $1 ), 1 );
+#        chop($res);
+#    } 
+#    $res =~ tr |' -_|AA-Za-z0-9+/|; 
+#    my $padding = ( 3 - length( $_[0] )% 3 )% 3; 
+#    $res =~ s/.{$padding}$/'=' x $padding/e if $padding; 
+#    return $res;
+#}
+
+
+
+    
+
---
+++ arduino/TweetM/wifi_config.h
@@ -0,0 +1,65 @@
+// BEGIN Wireless configuration parameters ----------------------------------
+// Edit these to match your network
+// You must pick an IP address by hand
+
+/*
+// crashspace network
+const prog_char ssid[] PROGMEM = {"CrashSpaceTW"};    // max 32 bytes
+unsigned char local_ip[]    = {172,16,16,182}; // IP address of WiShield
+unsigned char gateway_ip[]  = {172,16,16,1};   // router/gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};  // subnet mask for network
+// set security type
+unsigned char security_type = 0;    // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"blahblah"};  // max 64 chars
+*/
+/*
+// crashspace network
+const prog_char ssid[] PROGMEM = {"CrashSpaceDSL"};    // max 32 bytes
+unsigned char local_ip[]    = {192,168,1,182}; // IP address of WiShield
+unsigned char gateway_ip[]  = {192,168,1,254};   // router/gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};  // subnet mask for network
+// set security type
+unsigned char security_type = 0;    // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"blahblah"};  // max 64 chars
+*/
+/*
+// alex's network
+const prog_char ssid[] PROGMEM = {"The First"};    // max 32 bytes
+unsigned char local_ip[]    = {192,168,1,110}; // IP address of WiShield
+unsigned char gateway_ip[]  = {192,168,1,1};   // router/gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};  // subnet mask for network
+// set security type
+unsigned char security_type = 2;    // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"C0183rTn@t10N"};  // max 64 chars
+*/
+
+// tod's network
+const prog_char ssid[] PROGMEM = {"todbot"};    // max 32 bytes
+unsigned char local_ip[]    = {192,168,42,182}; // IP address of WiShield
+unsigned char gateway_ip[]  = {192,168,42,1};   // router/gateway IP address
+unsigned char subnet_mask[] = {255,255,255,0};  // subnet mask for network
+// set security type
+unsigned char security_type = 0;    // 0 - open; 1 - WEP; 2 - WPA; 3 - WPA2
+// WPA/WPA2 passphrase
+const prog_char security_passphrase[] PROGMEM = {"C0183rTn@t10N"};  // max 64 chars
+
+
+// WEP 128-bit keys
+// sample HEX keys
+prog_uchar wep_keys[] PROGMEM = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, // Key 0
+                                  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Key 1
+                                  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Key 2
+                                  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // Key 3
+};
+#define WIRELESS_MODE_INFRA 1
+#define WIRELESS_MODE_ADHOC 2
+// setup the wireless mode: 
+// infrastructure - connect to AP,  adhoc - connect to another WiFi device
+unsigned char wireless_mode = WIRELESS_MODE_INFRA;
+
+unsigned char ssid_len;
+unsigned char security_passphrase_len;
+// END   Wireless configuration parameters ----------------------------------
